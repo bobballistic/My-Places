fdf8b709b2d98ccd3ce06dcbb6882626045c154b
bobballistic <bologa.bogdan@gmailcom>
2014-11-05 22:00:44 +0000

    TableView Location From Place array at index active place ... Initial Commit

---
+++ MyPlaces/TableViewController.swift
@@ -0,0 +1,122 @@
+//
+//  TableViewController.swift
+//  MyPlaces
+//
+//  Created by Bob on 05/11/2014.
+//  Copyright (c) 2014 BallisticSoft. All rights reserved.
+//
+
+import UIKit
+
+var activePlace:Int = 0
+var places = [Dictionary<String,String>()]
+
+
+class TableViewController: UITableViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if places.count == 1 {
+            // We remove the empty cell at the begining of the table, not shure why it's showing ?!?!
+            places.removeAtIndex(0)
+        }
+        
+        places.append(["name":"Taj Mahal", "latitude":"27.175115", "longitude":"78.042144"])
+        places.append(["name":"Taj Mahal", "latitude":"27.175115", "longitude":"78.042144"])
+
+        
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // Moving to segue with identifier, and getting rid of navigation bar in map ?>
+        // What to do when row selected
+        activePlace = indexPath.row
+        
+        self.performSegueWithIdentifier("findPlace", sender: indexPath)
+        self.navigationController?.navigationBarHidden = true
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return places.count
+    }
+
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
+        // Populating the table
+        // Configure the cell...
+        cell.textLabel.text = places[indexPath.row]["name"]
+        return cell
+    }
+   
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -7,10 +7,29 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+
+
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet var myMap: MKMapView!
+    var manager = CLLocationManager()
 
     override func viewDidLoad() {
+        println(places[activePlace]["latitude"])
+        println(activePlace)
+        let theSpan = MKCoordinateSpanMake(0.01, 0.01)
+        // import lat and long from places at index activePlace, convert from sting to double
+        let lat = NSString(string: places[activePlace]["latitude"]!).doubleValue
+        let lon = NSString(string: places[activePlace]["longitude"]!).doubleValue
+    
+        
+        let location = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        let theRegion = MKCoordinateRegionMake(location, theSpan)
+        myMap.setRegion(theRegion, animated: true)
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
