c7d27a96fad5f04304f991d84a1d02e8d17d3d6c
bobballistic <bologa.bogdan@gmailcom>
2014-11-15 15:10:09 +0000

    Commit

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -77,8 +77,8 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         let theSpan = MKCoordinateSpanMake(0.01, 0.01)
         // import lat and long from places at index activePlace, convert from sting to double
-        let lat = NSString(string: places[activePlace]["lat"]!).doubleValue
-        let lon = NSString(string: places[activePlace]["lon"]!).doubleValue
+        let lat = NSString(string: places[activePlace]["latitude"]!).doubleValue
+        let lon = NSString(string: places[activePlace]["longitude"]!).doubleValue
          
           
          
